be905d5e8caaed955e5488062debb0172ca1b2d4
Andrew Waller <andrewhwaller@gmail.com>
2019-03-22 00:29:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-  new_song.artist_name = artist
+    new_song.artist_name = artist
     new_song.name = song_name
-    song
+    new_song
   end
 end
