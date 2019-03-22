8081fb4767a0519fa2ba7a728a644fd6168a32e3
Andrew Waller <andrewhwaller@gmail.com>
2019-03-22 00:27:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     artist_name = parts[0]
     song_name = parts[1].chomp(".mp3")
     song = self.new
-    song.artist_name = artist_name
+    song.artist_name = artist
     song.name = song_name
     song
   end
