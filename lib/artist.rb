43993b50c6c9a690925a5bb9404c7ae99f7cdaf7
Andrew Waller <andrewhwaller@gmail.com>
2019-03-22 01:32:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.find(name)
-    self.all.find { |name| artist.name == name }
+    self.all.find { |artist| artist.name == name }
   end
 
   def self.find_or_create_by_name(name)
