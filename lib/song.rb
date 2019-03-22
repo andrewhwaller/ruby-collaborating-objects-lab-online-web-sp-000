01aa60c4884c1ad7eabd4ba2e94d42fb9189b9ae
Andrew Waller <andrewhwaller@gmail.com>
2019-03-22 00:30:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist = artist
     new_song.name = song_name
     new_song
   end
