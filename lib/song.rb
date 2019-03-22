3c55265f0fe159a7a24456472287d8ec056da880
Andrew Waller <andrewhwaller@gmail.com>
2019-03-22 00:24:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,4 +5,15 @@ class Song
     @name = name
   end
 
+  def new_by_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].chomp(".mp3")
+    song = self.new
+    song.artist_name = artist_name
+    song.name = song_name
+    song
+end
+
+  end
 end
